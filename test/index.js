665e6a229c3ee0e34f522395f3e520b76580e435
Jeroen Engels <jfm.engels@gmail.com>
2016-10-15 12:21:45 +0200

    Migrate linting from standard to XO (#20)

--- test/index.js
+++ test/index.js
@@ -1,20 +1,21 @@
-import test from 'ava';
-import http from 'http';
-import listen from '../lib/';
-import request from 'request-promise';
+// eslint-disable-next-line ava/no-ignored-test-files
+import http from 'http'
+import test from 'ava'
+import request from 'request-promise'
+import listen from '../lib/'
 
-const srv = http.createServer((req, res) => res.end('1'));
-const srv2 = http.createServer((req, res) => res.end('2'));
+const srv = http.createServer((req, res) => res.end('1'))
+const srv2 = http.createServer((req, res) => res.end('2'))
 
 test('it works', async t => {
-  const url = await listen(srv);
-  t.ok(/http:\/\/localhost:\d+/.test(url));
-  const data = await request(url);
-  t.same('1', data);
+  const url = await listen(srv)
+  t.true(/http:\/\/localhost:\d+/.test(url))
+  const data = await request(url)
+  t.deepEqual('1', data)
 
-  const url2 = await listen(srv2);
-  t.ok(url2 !== url);
-  t.ok(/http:\/\/localhost:\d+/.test(url2));
-  const data2 = await request(url2);
-  t.same('2', data2);
-});
+  const url2 = await listen(srv2)
+  t.true(url2 !== url)
+  t.true(/http:\/\/localhost:\d+/.test(url2))
+  const data2 = await request(url2)
+  t.deepEqual('2', data2)
+})
